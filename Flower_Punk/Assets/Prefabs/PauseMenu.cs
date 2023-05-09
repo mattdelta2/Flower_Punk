d6fe8af43dbc9fb6bcf00dbbd0e6cd873d349337
Matthew Wierenga <matt.wierenga@outlook.com>
2023-05-09 12:19:24 +0200

    Added Pause Menu Prefab and all controls for it

---
+++ Flower_Punk/Assets/Prefabs/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            if(!pauseMenu.activeSelf)
+            {
+                Time.timeScale = 0f;
+                pauseMenu.SetActive(true);
+                Cursor.visible = true;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                pauseMenu.SetActive(false);
+                Cursor.visible = false;
+            }
+        }
+
+        
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+    }
+}
